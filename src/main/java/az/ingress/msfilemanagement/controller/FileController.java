cc74529071a9244d3e4fbc1ed33858cb7ee27612
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2023-06-24 17:47:51 +0400

    feat: adding json+file upload functionality

--- src/main/java/az/ingress/msfilemanagement/controller/FileController.java
+++ src/main/java/az/ingress/msfilemanagement/controller/FileController.java
@@ -4,7 +4,6 @@ import az.ingress.msfilemanagement.model.InformationDto;
 import az.ingress.msfilemanagement.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
