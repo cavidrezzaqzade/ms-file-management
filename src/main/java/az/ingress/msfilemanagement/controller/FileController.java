0828c425994c6b7c876adbf708c5b27e1369eae8
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2023-07-03 17:38:02 +0400

    feat: adding json+file upload functionality

--- src/main/java/az/ingress/msfilemanagement/controller/FileController.java
+++ src/main/java/az/ingress/msfilemanagement/controller/FileController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,9 +23,10 @@ public class FileController {
 
     private final FileService service;
 
-    @PostMapping(/*consumes = {"multipart/form-data"*//*, "application/json"*//*}*/)
-    @Operation(summary = "upload images", description = "upload images", tags = {"File"})
-    public ResponseEntity<?> uploadImage(@RequestPart("images") MultipartFile[] file, @RequestPart("info") InformationDto info){
+    @PostMapping
+    @Operation(summary = "upload files", description = "upload files", tags = {"File"})
+    public ResponseEntity<?> uploadImage(@RequestPart("files") MultipartFile[] file, @RequestPart("info") InformationDto info){
+        System.out.println("abcde");
         String uploadImage = service.uploadImage(file, info);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(uploadImage);
